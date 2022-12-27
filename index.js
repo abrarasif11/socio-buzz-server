8e7d486d88befee5d53adb0122375af474391c36
abrarasif11 <faasif929@gmail.com>
2022-12-27 22:34:51 +0600

    mongoDB-connected

--- index.js
+++ index.js
@@ -3,15 +3,31 @@ const cors = require('cors');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Middle Wares //
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.vhdpi0m.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+// MongoDB Collection //
+async function run() {
+    try {
+        
+    }
+    finally {
+
+    }
+}
+run().catch(err => console.error(err));
 app.get('/', (req, res) => {
     res.send('Socio Buzz Server is Running')
 })
-
 app.listen(port, () => {
     console.log(`Socio Buzz Server is Running on ${port}`);
 })
